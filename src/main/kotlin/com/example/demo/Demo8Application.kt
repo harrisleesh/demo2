6d376d3fe3ebf14468a9e766de81341b381eafa9
harris.yi <harris.yi@kakaostyle.com>
2023-05-25 13:49:07 +0900

    first commit

---
+++ src/main/kotlin/com/example/demo/Demo8Application.kt
@@ -0,0 +1,11 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Demo8Application
+
+fun main(args: Array<String>) {
+    runApplication<Demo8Application>(*args)
+}
