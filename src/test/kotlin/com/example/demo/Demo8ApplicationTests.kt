46d2b71e1e9a60a374cf9cd552a5e4ca92d4e7b6
harris.yi <harris.yi@kakaostyle.com>
2023-05-27 19:21:03 +0900

    feat: Add database dependency

--- src/test/kotlin/com/example/demo/Demo8ApplicationTests.kt
+++ src/test/kotlin/com/example/demo/Demo8ApplicationTests.kt
@@ -3,11 +3,4 @@ package com.example.demo
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class Demo8ApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-
-}
+class Demo8ApplicationTests {}
